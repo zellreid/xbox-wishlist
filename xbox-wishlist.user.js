0647abca2538ae516611acc732b51210ec792e46
zellreid <zellreid@gmail.com>
2025-11-22 15:02:42 +0200

    Bump version to 1.2.25326.5

--- xbox-wishlist.user.js
+++ xbox-wishlist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         XBOX Wishlist
 // @namespace    https://github.com/zellreid/xbox-wishlist
-// @version      1.2.25326.4
+// @version      1.2.25326.5
 // @description  Advanced filtering suite with Select2, tag display, inverted logic, and range sliders
 // @author       ZellReid
 // @homepage     https://github.com/zellreid/xbox-wishlist
